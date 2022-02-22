b0271b428d1ac45ffc471d59db3f53a1dcd91c24
MNtank <masternodetank@gmail.com>
2022-02-22 17:10:45 +0100

    remove old comments

--- bitcoin/params.go
+++ bitcoin/params.go
@@ -24,11 +24,8 @@ func init() {
 }
 
 var (
-	bigOne = big.NewInt(1)
-
-	PowLimit   = new(big.Int).Sub(new(big.Int).Lsh(bigOne, 20), bigOne)
-	PowLimitV1 = new(big.Int).Sub(new(big.Int).Lsh(bigOne, 24), bigOne)
-	PowLimitV2 = new(big.Int).Sub(new(big.Int).Lsh(bigOne, 20), bigOne)
+	bigOne   = big.NewInt(1)
+	PowLimit = new(big.Int).Sub(new(big.Int).Lsh(bigOne, 20), bigOne)
 )
 
 var EunoMainNetParams = chaincfg.Params{
@@ -56,16 +53,9 @@ var EunoMainNetParams = chaincfg.Params{
 	HDCoinType: 303,
 }
 
-// RegressionNetParams defines the network parameters for the regression test
-// Bitcoin network.  Not to be confused with the test Bitcoin network (version
-// 3), this network is sometimes simply called "testnet".
 var EunoRegressionNetParams = chaincfg.Params{
 	Name: "regtest",
 
-	// DigiByte has 0xdab5bffa as RegTest (same as Bitcoin's RegTest).
-	// Setting it to an arbitrary value (leet_hex(digibyte)), so that we can
-	// register the regtest network.
-	// DigiByte Core Developers will change this soon.
 	Net:         0xac7ecfa1,
 	DefaultPort: "46466",
 
@@ -78,16 +68,9 @@ var EunoRegressionNetParams = chaincfg.Params{
 	HDCoinType: 303,
 }
 
-// TestNet3Params defines the network parameters for the test Bitcoin network
-// (version 3).  Not to be confused with the regression test network, this
-// network is sometimes simply called "testnet".
 var EunoTestNet3Params = chaincfg.Params{
 	Name: "test",
 
-	// DigiByte has 0xdab5bffa as RegTest (same as Bitcoin's RegTest).
-	// Setting it to an arbitrary value (leet_hex(digibyte)), so that we can
-	// register the regtest network.
-	// DigiByte Core Developers will change this soon.
 	Net:         0xba657645,
 	DefaultPort: "46464",
 
@@ -96,18 +79,16 @@ var EunoTestNet3Params = chaincfg.Params{
 	GenesisHash:  &testNet3GenesisHash,
 
 	// Address encoding magics
-	PubKeyHashAddrID:        139,  // starts with 1
-	ScriptHashAddrID:        19,   // starts with 3
-	PrivateKeyID:            239,  // starts with 5 (uncompressed) or K (compressed)
-	WitnessPubKeyHashAddrID: 0x00, // starts with p2
-	WitnessScriptHashAddrID: 0x00, // starts with 7Xh
+	PubKeyHashAddrID:        139,
+	ScriptHashAddrID:        19,
+	PrivateKeyID:            239,
+	WitnessPubKeyHashAddrID: 0x00,
+	WitnessScriptHashAddrID: 0x00,
 
 	// BIP32 hierarchical deterministic extended key magics
-	HDPrivateKeyID: [4]byte{0x3a, 0x80, 0x58, 0x37}, // starts with xprv
-	HDPublicKeyID:  [4]byte{0x3a, 0x80, 0x61, 0xa0}, // starts with xpub
+	HDPrivateKeyID: [4]byte{0x3a, 0x80, 0x58, 0x37},
+	HDPublicKeyID:  [4]byte{0x3a, 0x80, 0x61, 0xa0},
 
-	// BIP44 coin type used in the hierarchical deterministic path for
-	// address generation.
 	HDCoinType: 303,
 }
 
